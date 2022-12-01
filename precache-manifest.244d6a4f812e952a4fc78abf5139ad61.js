4747abfec9419fd5e2e2741aca9a7c2caacdea21
Mujaddid Al Magribi <almagribi.appdev@gmail.com>
2022-12-01 20:53:29 +0700

    first

---
+++ precache-manifest.244d6a4f812e952a4fc78abf5139ad61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "923ef691f178ef7f5c54",
+    "url": "/static/css/main.20c40672.chunk.css"
+  },
+  {
+    "revision": "923ef691f178ef7f5c54",
+    "url": "/static/js/main.923ef691.chunk.js"
+  },
+  {
+    "revision": "82382150b0b5c16f6299",
+    "url": "/static/css/1.1c0d8ca5.chunk.css"
+  },
+  {
+    "revision": "82382150b0b5c16f6299",
+    "url": "/static/js/1.82382150.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1cb26b0b3328e983a9010789bbdc0e87",
+    "url": "/index.html"
+  }
+];
